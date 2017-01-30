80ea5f65b07e064e6c75d2c143dfdf6af88480b2
Mykhailo <staskopernik@gmail.com>
2017-01-30 09:27:14 +0000

    update files

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
-//document.write('Hello World!');
-//var hello = 'Hello World';
-//alert(hello);
-//console.log(hello);
+var a = Number(prompt('Enter  a'));
+var b = Number(prompt('Enter  b'));
+var c = Number(prompt('Enter  c'));
+var quadra = quadraticEquationSolution(a, b, c);
+document.write('<br>' + quadra);
 
-var userName = prompt('Enter your name');
-var greet = 'Hello ' + userName;
-document.write(greet);
-var userAge = Number(prompt('Enter your age'));
-if (isNaN(userAge)) {
-    document.write(' Please enter a number')
-    } else{
-    var nextAge = addOne(userAge);
-    document.write('<br> Soon you will be ' + nextAge);
+function quadraticEquationSolution(a, b, c) {
+    if (a !== 0) {
+        var D = b * b - 4 * a * c;
+        var result
+        if (D === 0) {
+            var x = -b / (2 * a);
+            result = 'x1 = x2 = ' + x;
+        }
+        else {
+            if (D > 0) {
+                var x1 = (-b + Math.sqrt(D)) / (2 * a);
+                var x2 = (-b - Math.sqrt(D)) / (2 * a);
+                result = 'x1 = ' + x1 + '; x2 = ' + x2;
+            }
+            else {
+                result = 'There is no solution';
+            }
+        }
+    } else {
+        if(b === 0){
+            result = 'There is no solution';
+        } else {
+            x = -c/b;
+            result = 'x1 = x2 = ' + x;
+        }
+    }
+    
+    document.write(result);
 }
-
-function addOne(someNum){
-    var result = someNum + 1;
-    return result;
-}
-Math.sqrt(4);
